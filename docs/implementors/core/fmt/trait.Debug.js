59d1e4b842ad83337d930d5abcae8179259706f1
tmandorfer <t.mandorfer@gmail.com>
2023-07-21 12:16:58 +0200

    feat: update docs

--- docs/implementors/core/fmt/trait.Debug.js
+++ docs/implementors/core/fmt/trait.Debug.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rust_miscellaneous_pkg":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"rust_miscellaneous_pkg/custom_request/custom_request/struct.CustomResponse.html\" title=\"struct rust_miscellaneous_pkg::custom_request::custom_request::CustomResponse\">CustomResponse</a>"]]
+"rust_miscellaneous_pkg":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"rust_miscellaneous_pkg/custom_request/custom_request/struct.CustomResponse.html\" title=\"struct rust_miscellaneous_pkg::custom_request::custom_request::CustomResponse\">CustomResponse</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
